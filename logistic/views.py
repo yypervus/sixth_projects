46b3e50c87b47ead6405dc685f3635d079aca685
Yuriy Pervushin <vp.russian@yandex.ru>
2022-01-23 17:21:51 +0500

    added files

---
+++ logistic/views.py
@@ -0,0 +1,23 @@
+from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.viewsets import ModelViewSet
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
